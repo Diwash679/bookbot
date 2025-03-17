d8b67d56e9e4e6d60872b6f437a0f9d50663bdd9
Diwash <diwashadhikari679@gmail.com>
2025-03-17 11:58:05 +0800

    main.py shows number of words

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        return f.read()
+
+def num_words():
+    f = get_book_text()
+    return len(f.split())
+
+def main():
+    text = get_book_text()
+    num = num_words()
+    print(text)
+    print(f"{num} words found in the document")
+
+main()
